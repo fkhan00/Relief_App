a0b5a5980ea12a23d233277854bedc84d5c0e6fd
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-04-20 09:16:57 -0400

    added sample warnings to csv

--- Driver.java
+++ Driver.java
@@ -1,8 +1,12 @@
 public class Driver{
   public static void main(String[] args){
-    Information[] ary = new Information[10];
-    for(int i = 0; i < 10; i++){
-      ary[i] = new Information(5, "heavy floods in Central", "8pm");}
+    Information[] ary = new Information[6];
+    ary[0] = new Information(8, "Food Rations Destroyed", "4/15/19");
+    ary[1] = new Information(6, "Relief Shelter set up in Connaught Place", "4/15/19");
+    ary[2] = new Information(7, "NMDA advises all citizens to stay at home", "4/16/19");
+    ary[3] = new Information(10, "Rescue Forces deployed in New Deli", "4/15/19");
+    ary[4] = new Information(8, "We are expecting 15 inches of rain", "4/16/19");
+    ary[5] = new Information(9, "Missing Child, Aryan Burhati, Age: 14, Curly hair, 5' 11, last seen in Colaba", "4/16/19");
     InfoDistribution sample = new InfoDistribution(ary);
     sample.update("main.csv");
 }}
